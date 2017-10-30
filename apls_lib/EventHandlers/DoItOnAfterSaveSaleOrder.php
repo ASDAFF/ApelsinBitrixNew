774d5eedf52c8f05637ae20492d38be331e04edd
Maxim Zaytsev <zaytsev.max90@gmail.com>
2017-10-30 16:57:03 +0300

    Исправление ошибки при которой нельзя было удалить заказ через админку.

--- apls_lib/EventHandlers/DoItOnAfterSaveSaleOrder.php
+++ apls_lib/EventHandlers/DoItOnAfterSaveSaleOrder.php
@@ -39,40 +39,42 @@ function SaleOrderDoItOnSave (Main\Event $event) {
                 foreach ($extraServices as $key => $extra) {
                     if(in_array($key,$ES_UP_TO_FLOR)) {
                         $EXTRA_SERVICES["UP_TO_FLOR"] = $extra;
-//                        AddMessage2Log("Подъем: ".$extra, "SaleOrderDoItOnSave");
                     }
                     if(in_array($key,$ES_FOR_TIME)) {
                         $EXTRA_SERVICES["FOR_TIME"] = $extra;
-//                        AddMessage2Log("Ко времени: ".$extra, "SaleOrderDoItOnSave");
                     }
                     if(in_array($key,$ES_KILOMETRS)) {
                         $EXTRA_SERVICES["KILOMETRS"] = $extra;
-//                        AddMessage2Log("Километраж: ".$extra, "SaleOrderDoItOnSave");
                     }
                 }
             }
         }
-
         $properties = $order->getPropertyCollection();
         $CARD_NUMBER = $properties->getItemByOrderPropertyId($CARD_NUMBER_ID);
-        $CARD_NUMBER->setValue($rsUserArr["UF_CARD_NUMBER"]);
-
+        if($CARD_NUMBER !== null) {
+            $CARD_NUMBER->setValue($rsUserArr["UF_CARD_NUMBER"]);
+        }
         $TYPE_PRICE = $properties->getItemByOrderPropertyId($TYPE_PRICE_ID);
-        $TYPE_PRICE->setValue($rsUserArr["UF_1C_TYPE_PRICE"]);
-
+        if($TYPE_PRICE !== null) {
+            $TYPE_PRICE->setValue($rsUserArr["UF_1C_TYPE_PRICE"]);
+        }
         if(isset($EXTRA_SERVICES["UP_TO_FLOR"])) {
             $UP_TO_FLOR = $properties->getItemByOrderPropertyId($UP_TO_FLOR_ID);
-            $UP_TO_FLOR->setValue($EXTRA_SERVICES["UP_TO_FLOR"]);
+            if($UP_TO_FLOR !== null) {
+                $UP_TO_FLOR->setValue($EXTRA_SERVICES["UP_TO_FLOR"]);
+            }
         }
-
         if(isset($EXTRA_SERVICES["FOR_TIME"])) {
             $FOR_TIME = $properties->getItemByOrderPropertyId($FOR_TIME_ID);
-            $FOR_TIME->setValue($EXTRA_SERVICES["FOR_TIME"]);
+            if($FOR_TIME !== null) {
+                $FOR_TIME->setValue($EXTRA_SERVICES["FOR_TIME"]);
+            }
         }
-
         if(isset($EXTRA_SERVICES["KILOMETRS"])) {
             $KILOMETRS = $properties->getItemByOrderPropertyId($KILOMETRS_ID);
-            $KILOMETRS->setValue($EXTRA_SERVICES["KILOMETRS"]);
+            if($KILOMETRS !== null) {
+                $KILOMETRS->setValue($EXTRA_SERVICES["KILOMETRS"]);
+            }
         }
     }
 }
