53344b46e4a3e2a214934fac3e16709f2c29d790
Aleks Melkov <alexmelkov@mail.ru>
2017-12-14 08:43:53 +0300

    Редактирование странички "Заказ сформирован"

--- bitrix/templates/elektro_flat_APLS/components/bitrix/sale.order.ajax/.default/confirm.php
+++ bitrix/templates/elektro_flat_APLS/components/bitrix/sale.order.ajax/.default/confirm.php
@@ -9,9 +9,9 @@ if($arParams["SET_TITLE"] == "Y") {
 if(!empty($arResult["ORDER"])) {?>
 	<p><?=Loc::getMessage("SOA_ORDER_SUC", array("#ORDER_DATE#" => $arResult["ORDER"]["DATE_INSERT"], "#ORDER_ID#" => $arResult["ORDER"]["ACCOUNT_NUMBER"]));?></p>
 	<?if(!empty($arResult["ORDER"]["PAYMENT_ID"])) {?>
-		<p><?=Loc::getMessage("SOA_PAYMENT_SUC", array("#PAYMENT_ID#" => $arResult["PAYMENT"][$arResult["ORDER"]["PAYMENT_ID"]]["ACCOUNT_NUMBER"]));?></p>
+<!--		<p>--><?//=Loc::getMessage("SOA_PAYMENT_SUC", array("#PAYMENT_ID#" => $arResult["PAYMENT"][$arResult["ORDER"]["PAYMENT_ID"]]["ACCOUNT_NUMBER"]));?><!--</p>-->
 	<?}?>
-	<p><?=Loc::getMessage("SOA_ORDER_SUC1", array("#LINK#" => $arParams["PATH_TO_PERSONAL"]))?></p>	
+	<p><?=Loc::getMessage("SOA_ORDER_SUC1", array("#LINK#" => $arParams["PATH_TO_PERSONAL"]))?></p>
 	
 	<?if($arResult["ORDER"]["IS_ALLOW_PAY"] === "Y") {
 		if(!empty($arResult["PAYMENT"])) {
@@ -23,9 +23,9 @@ if(!empty($arResult["ORDER"])) {?>
 							<table class="sale_order_full_table">
 								<tr>
 									<td class="ps_logo">
-										<div class="pay_name"><?=Loc::getMessage("SOA_PAY")?></div>
-										<?=CFile::ShowImage($arPaySystem["LOGOTIP"], 100, 100, "border=0\" style=\"width:100px\"", "", false);?>
-										<div class="paysystem_name"><?=$arPaySystem["NAME"]?></div>
+<!--										<div class="pay_name">--><?//=Loc::getMessage("SOA_PAY")?><!--</div>-->
+<!--										--><?//=CFile::ShowImage($arPaySystem["LOGOTIP"], 100, 100, "border=0\" style=\"width:100px\"", "", false);?>
+<!--										<div class="paysystem_name">--><?//=$arPaySystem["NAME"]?><!--</div>-->
 										<br/>
 									</td>
 								</tr>
