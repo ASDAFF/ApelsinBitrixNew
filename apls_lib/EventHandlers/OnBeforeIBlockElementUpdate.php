e5da1fb4680c1aa58a35c4b63a520d7ff44c138f
Aleks Melkov <alexmelkov@mail.ru>
2017-12-20 10:32:49 +0300

    Обновление события проставления активности (галка "товар проверен")

--- apls_lib/EventHandlers/OnBeforeIBlockElementUpdate.php
+++ apls_lib/EventHandlers/OnBeforeIBlockElementUpdate.php
@@ -7,6 +7,7 @@ class APLS_ActivateUpdater
 {
     const SITE_DEL_SECTION_XML_ID = "98557c36-f99a-11e6-80ec-00155dfef48a";
     const AKTIVNOST_XML_ID = "26e05687-c602-4c36-8b63-debb1b4e0250";
+    const CHECKEDPRODUCT_XML_ID = "26e05687-c602-4c36-8b63-desc5g0XN322";
     const KOEFF_XML_ID = "26e05687-c602-4c36-8b63-debb3g4eu248";
     const XML_ARRAY = array(
         "KOEFF" => "26e05687-c602-4c36-8b63-debb3g4eu248",
@@ -22,6 +23,8 @@ class APLS_ActivateUpdater
     private static $entity_data_class;
     private static $aktivnost_id;
     private static $aktivnost_code;
+    private static $checkedProduct_code;
+    private static $checkedProduct_id;
     private static $dimensions_koeff_id = array();
     private static $dimensions_id = array();
     private static $siteDelSectionId;
@@ -35,6 +38,8 @@ class APLS_ActivateUpdater
         }
         self::$aktivnost_id = APLS_CatalogProperties::convertPropertyXMLIDtoID(self::AKTIVNOST_XML_ID);
         self::$aktivnost_code = APLS_CatalogProperties::convertPropertyXMLIDtoCODE(self::AKTIVNOST_XML_ID);
+        self::$checkedProduct_id = APLS_CatalogProperties::convertPropertyXMLIDtoID(self::CHECKEDPRODUCT_XML_ID);
+        self::$checkedProduct_code = APLS_CatalogProperties::convertPropertyXMLIDtoCODE(self::CHECKEDPRODUCT_XML_ID);
         self::$dimensions_koeff_id = APLS_CatalogProperties::convertPropertyXMLIDtoID(self::KOEFF_XML_ID);
         foreach (self::XML_ARRAY as $key => $code) {
             self::$dimensions_id[$key] = APLS_CatalogProperties::convertPropertyXMLIDtoID($code);
@@ -59,43 +64,47 @@ class APLS_ActivateUpdater
         if ($res_arr['IBLOCK_SECTION_ID'] === self::$siteDelSectionId && $res_arr["ACTIVE"] === "Y") {
             $arFields["ACTIVE"] = "N";
         } else {
-            $arr = CIBlockElement::GetProperty($arFields["IBLOCK_ID"], $arFields["ID"], array(), array("CODE" => self::$aktivnost_code));
-            if ($props = $arr->Fetch()) {
-                if (
-                    self::getArrayPropertyValue($arFields,self::$aktivnost_id) !== $props["VALUE"] &&
-                    self::getArrayPropertyValue($arFields,self::$aktivnost_id) !== NULL
-                ) {
-                    $yes = "false"; $no = "true";
+            $property_enums = CIBlockPropertyEnum::GetList(Array("DEF"=>"DESC", "SORT"=>"ASC"), Array("IBLOCK_ID"=>$arFields["IBLOCK_ID"], "CODE"=>self::$checkedProduct_code));
+            $options = array();
+            while($enum_fields = $property_enums->GetNext())
+            {
+                $options[$enum_fields["ID"]] = $enum_fields["XML_ID"];
+            }
+                if ($options[self::getArrayPropertyValue($arFields, self::$checkedProduct_id)] == "true") {
+                    $arr_akt = CIBlockElement::GetProperty($arFields["IBLOCK_ID"], $arFields["ID"], array(), array("CODE" => self::$aktivnost_code));
+                    if ($prop_akt = $arr_akt->Fetch()) {
+                        if (
+                            self::getArrayPropertyValue($arFields, self::$aktivnost_id) !== $prop_akt["VALUE"] &&
+                            self::getArrayPropertyValue($arFields, self::$aktivnost_id) !== NULL
+                        ) {
+                            $yes = "false";
+                            $no = "true";
+                        } else {
+                            $yes = "true";
+                            $no = "false";
+                        }
+                        if ($activeValue === "N" && $prop_akt["VALUE_XML_ID"] === $yes) {
+                            $arFields["ACTIVE"] = "Y";
+                        } elseif ($activeValue === "Y" && $prop_akt["VALUE_XML_ID"] === $no) {
+                            $arFields["ACTIVE"] = "N";
+                        }
+                    }
                 } else {
-                    $yes = "true"; $no = "false";
-                }
-                if ($activeValue === "N" && $props["VALUE_XML_ID"] === $yes) {
-                    $arFields["ACTIVE"] = "Y";
-                } elseif ($activeValue === "Y" && $props["VALUE_XML_ID"] === $no) {
                     $arFields["ACTIVE"] = "N";
                 }
-            }
         }
+
         $updateArray = array();
         //Получение значений размеров из торгового каталога товара
         $dimensionsCatalogValues = self::getCatalogValue($arFields["ID"]);
         $dimensionsCatalogValues["KOEFF"] = self::getCoefficientValue($arFields["IBLOCK_ID"], $arFields["ID"]);
         $dimensionsHighloadValue = self::getHLbValue($arFields["XML_ID"]);
-//        echo "<pre>";
-//        var_dump($dimensionsHighloadValue);
-//        die;
         //Проверка необходимости обновления размеров и коэффициэнта
         foreach (self::XML_ARRAY as $key => $value) {
-            $ufkey = "UF_".$key;
-            $dimensionsEditValue = self::getArrayPropertyValue($arFields,self::$dimensions_id[$key]);
-
-//            echo "<pre>";
-//            var_dump($dimensionsEditValue);
-//            var_dump($dimensionsCatalogValues[$key]);
-//            var_dump($dimensionsHighloadValue[$ufkey]);
-
+            $ufkey = "UF_" . $key;
+            $dimensionsEditValue = self::getArrayPropertyValue($arFields, self::$dimensions_id[$key]);
             if (self::checkEmptyValue($dimensionsEditValue)) {
-                if(
+                if (
                     (!self::checkEmptyValue($dimensionsHighloadValue[$ufkey]) && !self::checkEmptyValue($dimensionsCatalogValues[$key])) ||
                     (!self::checkEmptyValue($dimensionsHighloadValue[$ufkey]) && $dimensionsCatalogValues[$key] !== $dimensionsEditValue) ||
                     (!self::checkEmptyValue($dimensionsCatalogValues[$key]) && $dimensionsHighloadValue[$ufkey] !== $dimensionsEditValue) ||
@@ -108,7 +117,6 @@ class APLS_ActivateUpdater
                     ($dimensionsCatalogValues[$key] === $dimensionsEditValue && $dimensionsHighloadValue[$ufkey] === $dimensionsEditValue) ||
                     ($dimensionsCatalogValues[$key] === $dimensionsEditValue && $dimensionsHighloadValue[$ufkey] !== $dimensionsEditValue)
                 ) {
-//                    var_dump(null);
                     $updateArray["UF_XMLID"] = $arFields["XML_ID"];
                     $updateArray[$ufkey] = null;
                 }
@@ -143,43 +151,43 @@ class APLS_ActivateUpdater
 //                $updateArray["UF_" . $key] = $dimensionsEditValue;
             }
         }
-//        var_dump($updateArray);
-//        die;
-        self::setHLbValue($updateArray,$arFields["XML_ID"]);
+        self::setHLbValue($updateArray, $arFields["XML_ID"]);
     }
 
-    private static function checkEmptyValue($val) {
+    private static function checkEmptyValue($val)
+    {
         return $val !== NULL && $val != 0 && trim($val) !== "";
     }
 
-    private static function getArrayPropertyValue($arFields,$key)
+    private static function getArrayPropertyValue($arFields, $key)
     {
         $val = array_shift(array_values($arFields["PROPERTY_VALUES"][$key]));
         return $val["VALUE"];
     }
 
-    private static function setHLbValue($updateArray,$xmlId)
+    private static function setHLbValue($updateArray, $xmlId)
     {
         if (!empty($updateArray)) {
             $rsData = self::$entity_data_class::getList(array(
                 "select" => array('ID'), //выбираем все поля
-                "filter" => ["UF_XMLID"=>$xmlId],
-                "order" => array("ID"=>"ASC") // сортировка по полю UF_SORT, будет работать только, если вы завели такое поле в hl'блоке
+                "filter" => ["UF_XMLID" => $xmlId],
+                "order" => array("ID" => "ASC") // сортировка по полю UF_SORT, будет работать только, если вы завели такое поле в hl'блоке
             ));
-            while($arRes = $rsData->Fetch()){
+            while ($arRes = $rsData->Fetch()) {
                 return self::$entity_data_class::update($arRes["ID"], $updateArray);
             }
             return self::$entity_data_class::add($updateArray);
         }
     }
 
-    private static function getHLbValue($xmlId) {
+    private static function getHLbValue($xmlId)
+    {
         $rsData = self::$entity_data_class::getList(array(
             "select" => array('*'), //выбираем все поля
-            "filter" => ["UF_XMLID"=>$xmlId],
-            "order" => array("ID"=>"ASC") // сортировка по полю UF_SORT, будет работать только, если вы завели такое поле в hl'блоке
+            "filter" => ["UF_XMLID" => $xmlId],
+            "order" => array("ID" => "ASC") // сортировка по полю UF_SORT, будет работать только, если вы завели такое поле в hl'блоке
         ));
-        while($arRes = $rsData->Fetch()){
+        while ($arRes = $rsData->Fetch()) {
             return $arRes;
         }
         return array();
