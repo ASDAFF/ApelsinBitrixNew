091ac251407a60142db2d729005b68f06bdb61ee
Aleks Melkov <alexmelkov@mail.ru>
2018-01-17 10:45:52 +0300

    Убираем favicon из вкладки браузера (верно)

--- bitrix/templates/elektro_flat_APLS/header.php
+++ bitrix/templates/elektro_flat_APLS/header.php
@@ -5,7 +5,7 @@ Loc::loadMessages(__FILE__);?>
 <!DOCTYPE html>
 <html lang="<?=LANGUAGE_ID?>">
 <head>
-<!--	<link rel="shortcut icon" type="image/x-icon" href="--><?//=SITE_TEMPLATE_PATH?><!--/favicon.ico" />	-->
+	<link rel="shortcut icon" type="image/x-icon" href="<?=SITE_TEMPLATE_PATH?>/favicon.ico" />	
 	<link rel="apple-touch-icon" sizes="57x57" href="<?=SITE_TEMPLATE_PATH?>/images/apple-touch-icon-114.png" />
 	<link rel="apple-touch-icon" sizes="114x114" href="<?=SITE_TEMPLATE_PATH?>/images/apple-touch-icon-114.png" />
 	<link rel="apple-touch-icon" sizes="72x72" href="<?=SITE_TEMPLATE_PATH?>/images/apple-touch-icon-144.png" />
