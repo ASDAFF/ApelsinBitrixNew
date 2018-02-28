157e1c89ed0ed3be78d32a630e4edcce6267e28b
Maxim Zaytsev <zaytsev.max90@gmail.com>
2018-02-28 12:35:35 +0300

    rc.apelsin.ru

--- include/geolocation.php
+++ include/geolocation.php
@@ -1,5 +1,7 @@
-<?$APPLICATION->IncludeComponent("altop:geolocation", "",
-	array(									
+<?$APPLICATION->IncludeComponent(
+	"altop:geolocation", 
+	".default", 
+	array(
 		"IBLOCK_TYPE" => "content",
 		"IBLOCK_ID" => "7",
 		"SHOW_CONFIRM" => "Y",
@@ -9,7 +11,9 @@
 		"TEXT_BLOCK_TITLE" => "",
 		"CACHE_TYPE" => "A",
 		"CACHE_TIME" => "36000000",
-		"COOKIE_TIME" => "36000000"
+		"COOKIE_TIME" => "36000000",
+		"COMPONENT_TEMPLATE" => ".default",
+		"MODE_OPERATION" => "YANDEX"
 	),
 	false
 );?>
