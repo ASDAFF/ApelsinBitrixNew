93346876200b3ee90433194deb909991e2290771
Maxim Zaytsev <zaytsev.max90@gmail.com>
2018-04-15 15:23:19 +0300

    поиск по изображениям в модели

--- apls_lib/promotions/model/PromotionImageModel.php
+++ apls_lib/promotions/model/PromotionImageModel.php
@@ -5,4 +5,37 @@ class PromotionImageModel extends PromotionModelAbstract
 {
     protected static $tableName = "apls_promotions_image";
     protected static $requiredFields = array('b_file_name','s_file_name','name','type');
+
+    public static function imageSearchByType($typeId, $searchString = "", $atLeastOne = true):array {
+        $searchWords = array();
+        if($searchString !== "") {
+            $searchString = mb_strtolower($searchString);
+            $searchWords = array_diff(explode(" ", $searchString),array(" "));
+        }
+        $whereType = MySQLWhereElementString::getBinaryOperationString(
+            'type',
+            MySQLWhereElementString::OPERATOR_B_EQUAL,
+            $typeId
+        );
+        $orderByObj = new MySQLOrderByString();
+        $orderByObj->add('name',MySQLOrderByString::ASC);
+        $images = PromotionImageModel::getElementList($whereType,null,null,$orderByObj);
+        if(!empty($searchWords)) {
+            foreach ($images as $imageId => $image) {
+                if($image instanceof PromotionImageModel) {
+                    $searchCount = 0;
+                    foreach ($searchWords as $searchWord) {
+                        $searchWord = str_replace("_"," ", $searchWord);
+                        if(substr_count(mb_strtolower($image->getFieldValue('name')),$searchWord) > 0) {
+                            $searchCount ++;
+                        }
+                    }
+                    if(($atLeastOne && $searchCount < 1) || (!$atLeastOne && $searchCount < count($searchWords))) {
+                        unset($images[$imageId]);
+                    }
+                }
+            }
+        }
+        return $images;
+    }
 }
