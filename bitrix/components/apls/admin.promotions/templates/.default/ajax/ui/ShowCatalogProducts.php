c94cf3613dbcd7838b7dcf94f24dac5e67fb554f
Aleks Melkov <alexmelkov@mail.ru>
2018-04-15 16:08:19 +0300

    Исправление вывода товаров в "добавленные позиции"

--- bitrix/components/apls/admin.promotions/templates/.default/ajax/ui/ShowCatalogProducts.php
+++ bitrix/components/apls/admin.promotions/templates/.default/ajax/ui/ShowCatalogProducts.php
@@ -4,6 +4,7 @@ define("NOT_CHECK_PERMISSIONS", true);
 require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 include_once $_SERVER["DOCUMENT_ROOT"] . "/apls_lib/promotions/model/PromotionRevisionModel.php";
 include_once $_SERVER["DOCUMENT_ROOT"] . "/apls_lib/catalog/sections/APLS_CatalogSections.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/apls_lib/catalog/models/CatalogElementModel.php";
 $revision = new PromotionRevisionModel($_REQUEST['revisionId']);
 $revisionProducts = array();
 if($_REQUEST['type'] === "product") {
@@ -13,16 +14,27 @@ if($_REQUEST['type'] === "product") {
 }
 ?>
 <div class="ListOfElements TwoColumns">
-    <?foreach ($revisionProducts as $revisionProduct):
-        if($revisionProduct instanceof PromotionCatalogProduct || $revisionProduct instanceof PromotionCatalogException):
-            $productXmlId = $revisionProduct->getFieldValue('product');
-            ?>
+    <?foreach ($revisionProducts as $revisionProduct):?>
+        <?if($revisionProduct instanceof PromotionCatalogProduct || $revisionProduct instanceof PromotionCatalogException):?>
+            <?$productXmlId = $revisionProduct->getFieldValue('product');?>
+            <?$products = CatalogElementModel::getElementList(
+                    MySQLWhereElementString::getBinaryOperationString(
+                            'XML_ID',
+                            MySQLWhereElementString::OPERATOR_B_EQUAL,
+                            $productXmlId
+                    )
+            )?>
+            <?$product = array_shift($products);?>
             <div class="ElementBlock">
                 <div class="ElementBlockContent" tableId="<?=$revisionProduct->getId()?>" type="<?=$_REQUEST['type']?>">
-                    <div class="content"><?=$productXmlId?></div>
+                    <?if($product instanceof CatalogElementModel):?>
+                        <div class="content"><?=$product->getFieldValue("NAME")?></div>
+                    <?else:?>
+                        <div class="content"><?=$productXmlId?></div>
+                    <?endif;?>
                     <div class="DellButton"></div>
                 </div>
             </div>
-        <?endif;
-    endforeach;?>
+        <?endif;?>
+    <?endforeach;?>
 </div>
