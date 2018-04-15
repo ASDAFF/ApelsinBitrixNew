9e181fff59bbe6fe674b7914a1b1370bf64c2abe
Maxim Zaytsev <zaytsev.max90@gmail.com>
2018-04-15 16:48:55 +0300

    Поиск дял модели PromotionImageInRevisionModel

--- apls_lib/promotions/model/PromotionImageInRevisionModel.php
+++ apls_lib/promotions/model/PromotionImageInRevisionModel.php
@@ -5,4 +5,26 @@ class PromotionImageInRevisionModel extends PromotionModelAbstract
 {
     protected static $tableName = "apls_promotions_image_in_revision";
     protected static $requiredFields = array('revision','img');
+
+    public static function searchByRevision(string $revisionId): array
+    {
+        return PromotionImageInRevisionModel::getElementList(
+            MySQLWhereElementString::getBinaryOperationString(
+                'revision',
+                MySQLWhereElementString::OPERATOR_B_EQUAL,
+                $revisionId
+            )
+        );
+    }
+
+    public static function searchByImage(string $imageId): array
+    {
+        return PromotionImageInRevisionModel::getElementList(
+            MySQLWhereElementString::getBinaryOperationString(
+                'img',
+                MySQLWhereElementString::OPERATOR_B_EQUAL,
+                $imageId
+            )
+        );
+    }
 }
