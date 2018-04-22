cf14d8e9fc283d79e044228cb5b33801cbf74f17
Maxim Zaytsev <zaytsev.max90@gmail.com>
2018-04-22 14:53:44 +0300

    Добавление поиска по алиасу в PromotionImageTypeModel

--- apls_lib/promotions/model/PromotionImageTypeModel.php
+++ apls_lib/promotions/model/PromotionImageTypeModel.php
@@ -5,4 +5,19 @@ class PromotionImageTypeModel extends PromotionModelAbstract
 {
     protected static $tableName = "apls_promotions_image_type";
     protected static $requiredFields = array('alias','type');
+
+    public static function searchTypeByAlias($alias) {
+        $list = PromotionImageTypeModel::getElementList(
+            MySQLWhereElementString::getBinaryOperationString(
+                'alias',
+                MySQLWhereElementString::OPERATOR_B_EQUAL,
+                $alias
+            )
+        );
+        if(empty($list)) {
+            return null;
+        } else {
+            return array_shift($list);
+        }
+    }
 }
