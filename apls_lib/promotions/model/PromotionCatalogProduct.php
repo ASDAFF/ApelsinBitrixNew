88711d0ba80f56ec179d504cfdc3b1d35253965a
Maxim Zaytsev <zaytsev.max90@gmail.com>
2018-04-29 11:29:56 +0300

    добавление поиска по ервизии

--- apls_lib/promotions/model/PromotionCatalogProduct.php
+++ apls_lib/promotions/model/PromotionCatalogProduct.php
@@ -5,4 +5,15 @@ class PromotionCatalogProduct extends PromotionModelAbstract
 {
     protected static $tableName = "apls_promotions_revision_catalog_products";
     protected static $requiredFields = array('revision', 'product');
+
+    public static function searchByRevision(string $revisionId): array
+    {
+        return PromotionCatalogProduct::getElementList(
+            MySQLWhereElementString::getBinaryOperationString(
+                'revision',
+                MySQLWhereElementString::OPERATOR_B_EQUAL,
+                $revisionId
+            )
+        );
+    }
 }
