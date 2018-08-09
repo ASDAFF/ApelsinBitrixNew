6d4d206be74d29611fb534d9cd299640536e0e7c
Maxim Zaytsev <zaytsev.max90@gmail.com>
2018-08-09 15:06:55 +0300

    Изменение заголовка popap окна выбора региона

--- bitrix/components/altop/geolocation/templates/.default/lang/ru/template.php
+++ bitrix/components/altop/geolocation/templates/.default/lang/ru/template.php
@@ -4,5 +4,5 @@ $MESS["GEOLOCATION_NOT_DEFINED"] = "Не определен";
 $MESS["GEOLOCATION_YOUR_CITY"] = "Ваш город";
 $MESS["GEOLOCATION_YES"] = "Да";
 $MESS["GEOLOCATION_CHANGE_CITY"] = "Выбрать другой город";
-$MESS["GEOLOCATION_POPUP_WINDOW_TITLE"] = "Ваш регион доставки";
+$MESS["GEOLOCATION_POPUP_WINDOW_TITLE"] = "Ваш регион";
 ?>
