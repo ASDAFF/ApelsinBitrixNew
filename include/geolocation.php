400332cd342e2bfd5f0ee7e6f916d0696d1d264f
Maxim Zaytsev <zaytsev.max90@gmail.com>
2018-08-09 15:23:10 +0300

    отключаем футер у окна выбора города

--- include/geolocation.php
+++ include/geolocation.php
@@ -6,7 +6,7 @@
 		"IBLOCK_ID" => "7",
 		"SHOW_CONFIRM" => "Y",
 		"SHOW_DEFAULT_LOCATIONS" => "Y",
-		"SHOW_TEXT_BLOCK" => "Y",
+		"SHOW_TEXT_BLOCK" => "N",
 		"SHOW_TEXT_BLOCK_TITLE" => "Y",
 		"TEXT_BLOCK_TITLE" => "",
 		"CACHE_TYPE" => "A",
