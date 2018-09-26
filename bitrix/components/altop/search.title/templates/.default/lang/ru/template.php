e2c913ed5fdefb9056c12a85436545ee9bc52394
Maxim Zaytsev <zaytsev.max90@gmail.com>
2018-09-26 10:24:31 +0300

    Сменить текст в поле поиска

--- bitrix/components/altop/search.title/templates/.default/lang/ru/template.php
+++ bitrix/components/altop/search.title/templates/.default/lang/ru/template.php
@@ -1,4 +1,4 @@
 <?
-$MESS["ALTOP_CATALOG_SEARCH"] = "Поиск по товарам, брендам, категориям";
+$MESS["ALTOP_CATALOG_SEARCH"] = "Поиск по 45000 товаров в наличии";
 $MESS["ALTOP_SEARCH_BUTTON"] = "Найти";
 ?>
