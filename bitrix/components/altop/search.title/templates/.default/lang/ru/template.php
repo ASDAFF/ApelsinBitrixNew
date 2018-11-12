53283e7d89dda320a6ea84fd4dbaa7214bfccbeb
Maxim Zaytsev <zaytsev.max90@gmail.com>
2018-11-12 10:45:04 +0300

    70 000 товаров вместо 45 000

--- bitrix/components/altop/search.title/templates/.default/lang/ru/template.php
+++ bitrix/components/altop/search.title/templates/.default/lang/ru/template.php
@@ -1,4 +1,4 @@
 <?
-$MESS["ALTOP_CATALOG_SEARCH"] = "Поиск среди 45000 товаров в наличии";
+$MESS["ALTOP_CATALOG_SEARCH"] = "Поиск среди 70000 товаров в наличии";
 $MESS["ALTOP_SEARCH_BUTTON"] = "Найти";
 ?>
