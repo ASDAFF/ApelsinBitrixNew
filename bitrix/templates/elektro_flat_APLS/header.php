e7efcb993806908c48d98d16d8537fb2cf123fac
Aleks Melkov <alexmelkov@mail.ru>
2019-03-04 09:06:25 +0300

    Изменение по иконкам для iPhone

--- bitrix/templates/elektro_flat_APLS/header.php
+++ bitrix/templates/elektro_flat_APLS/header.php
@@ -6,10 +6,9 @@ Loc::loadMessages(__FILE__);?>
 <html lang="<?=LANGUAGE_ID?>">
 <head>
 	<link rel="shortcut icon" type="image/x-icon" href="<?=SITE_TEMPLATE_PATH?>/favicon.ico" />	
-	<link rel="apple-touch-icon" sizes="57x57" href="<?=SITE_TEMPLATE_PATH?>/images/apple-touch-icon-114.png" />
-	<link rel="apple-touch-icon" sizes="114x114" href="<?=SITE_TEMPLATE_PATH?>/images/apple-touch-icon-114.png" />
-	<link rel="apple-touch-icon" sizes="72x72" href="<?=SITE_TEMPLATE_PATH?>/images/apple-touch-icon-144.png" />
-	<link rel="apple-touch-icon" sizes="144x144" href="<?=SITE_TEMPLATE_PATH?>/images/apple-touch-icon-144.png" />
+	<link rel="apple-touch-icon" sizes="57x57" href="<?=SITE_TEMPLATE_PATH?>/images/apelsin_touch_icon_57.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<?=SITE_TEMPLATE_PATH?>/images/apelsin_touch_icon_114.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="<?=SITE_TEMPLATE_PATH?>/images/apelsin_touch_icon_72.png" />
     <script src="//api-maps.yandex.ru/2.1/?lang=ru-RU" type="text/javascript"></script>
 	<?Asset::getInstance()->addString("<meta name='viewport' content='width=device-width, initial-scale=1.0' />");?>
 	<title><?$APPLICATION->ShowTitle()?></title>
