9f58519ed835ba3076a6ef2f2dc6d3a640ce5fbb
Aleks Melkov <alexmelkov@mail.ru>
2019-03-20 13:03:20 +0300

    Правка страницы Особенности товаров

--- include/services/USTANOVKA.php
+++ include/services/USTANOVKA.php
@@ -1,4 +1,4 @@
 <div class="FeaturesProductsTextBlock">
-    <div class="FeaturesProductsIcon"><img src="/apls_resources/DeliveryIcons/assembling.svg"></div>
+    <div class="FeaturesProductsName">Установка</div>
     <div class="FeaturesProductsText">Возможен заказ установки или монтажа.</div>
 </div>
