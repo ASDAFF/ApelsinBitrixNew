e6a58ab92f0ad8689b5a3302fe663a98e21c52d7
Aleks Melkov <alexmelkov@mail.ru>
2019-03-20 08:43:27 +0300

    Изменение вывода левых баннеров

--- include/banners_left.php
+++ include/banners_left.php
@@ -1,48 +1,68 @@
-<?$APPLICATION->IncludeComponent("bitrix:news.list", "banners_left",
-	array(
-		"IBLOCK_TYPE" => "content",
-		"IBLOCK_ID" => "6",
-		"NEWS_COUNT" => "10",
-		"SORT_BY1" => "SORT",
-		"SORT_ORDER1" => "ASC",
-		"SORT_BY2" => "ACTIVE_FROM",
-		"SORT_ORDER2" => "DESC",
-		"FILTER_NAME" => "",
-		"FIELD_CODE" => array(),
-		"PROPERTY_CODE" => array(
-			0 => "URL"
-		),
-		"CHECK_DATES" => "Y",
-		"DETAIL_URL" => "",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_SHADOW" => "Y",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"AJAX_OPTION_HISTORY" => "N",
-		"CACHE_TYPE" => "A",
-		"CACHE_TIME" => "36000000",
-		"CACHE_FILTER" => "Y",
-		"CACHE_GROUPS" => "Y",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"DISPLAY_PANEL" => "N",
-		"SET_TITLE" => "N",
-		"SET_STATUS_404" => "N",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-		"ADD_SECTIONS_CHAIN" => "N",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"PARENT_SECTION" => "",
-		"PARENT_SECTION_CODE" => "",
-		"DISPLAY_NAME" => "N",
-		"DISPLAY_DATE" => "N",
-		"DISPLAY_TOP_PAGER" => "N",
-		"DISPLAY_BOTTOM_PAGER" => "N",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => "",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "3600",
-		"PAGER_SHOW_ALL" => "N",
-		"AJAX_OPTION_ADDITIONAL" => ""
-	),
-	false
-);?>
+<?//$APPLICATION->IncludeComponent("bitrix:news.list", "banners_left",
+//	array(
+//		"IBLOCK_TYPE" => "content",
+//		"IBLOCK_ID" => "6",
+//		"NEWS_COUNT" => "10",
+//		"SORT_BY1" => "SORT",
+//		"SORT_ORDER1" => "ASC",
+//		"SORT_BY2" => "ACTIVE_FROM",
+//		"SORT_ORDER2" => "DESC",
+//		"FILTER_NAME" => "",
+//		"FIELD_CODE" => array(),
+//		"PROPERTY_CODE" => array(
+//			0 => "URL"
+//		),
+//		"CHECK_DATES" => "Y",
+//		"DETAIL_URL" => "",
+//		"AJAX_MODE" => "N",
+//		"AJAX_OPTION_SHADOW" => "Y",
+//		"AJAX_OPTION_JUMP" => "N",
+//		"AJAX_OPTION_STYLE" => "Y",
+//		"AJAX_OPTION_HISTORY" => "N",
+//		"CACHE_TYPE" => "A",
+//		"CACHE_TIME" => "36000000",
+//		"CACHE_FILTER" => "Y",
+//		"CACHE_GROUPS" => "Y",
+//		"PREVIEW_TRUNCATE_LEN" => "",
+//		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+//		"DISPLAY_PANEL" => "N",
+//		"SET_TITLE" => "N",
+//		"SET_STATUS_404" => "N",
+//		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+//		"ADD_SECTIONS_CHAIN" => "N",
+//		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+//		"PARENT_SECTION" => "",
+//		"PARENT_SECTION_CODE" => "",
+//		"DISPLAY_NAME" => "N",
+//		"DISPLAY_DATE" => "N",
+//		"DISPLAY_TOP_PAGER" => "N",
+//		"DISPLAY_BOTTOM_PAGER" => "N",
+//		"PAGER_SHOW_ALWAYS" => "N",
+//		"PAGER_TEMPLATE" => "",
+//		"PAGER_DESC_NUMBERING" => "N",
+//		"PAGER_DESC_NUMBERING_CACHE_TIME" => "3600",
+//		"PAGER_SHOW_ALL" => "N",
+//		"AJAX_OPTION_ADDITIONAL" => ""
+//	),
+//	false
+//);?>
+<div class="banners_left">
+    <a class="banners_left_toRent" href="/information/to_rent/">
+        <div class="banners_left_text">
+            <div class="banners_left_text_1">Ищем помещение</div>
+            <div class="banners_left_text_2">8(920)970-07-01</div>
+        </div>
+    </a>
+    <a class="banners_left_ustanovka" href="/promotions/id/CCAKBMIOABEG7-B8IZB2F4D1DNX-FUJBJJG9A/">
+        <div class="banners_left_text">
+            <div class="banners_left_text_1">Установка металлической двери</div>
+            <div class="banners_left_text_2">в <span class="up">подарок</span></div>
+        </div>
+    </a>
+    <a class="banners_left_consult" href="/information/consultation/">
+        <div class="banners_left_text">
+            <div class="banners_left_text_1">Квалифицированная</div>
+            <div class="banners_left_text_2"><span class="up">Консультация</span> и <span class="up">расчет</span></div>
+        </div>
+    </a>
+</div>
