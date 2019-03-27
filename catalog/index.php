48bdf19b6483e584f5e17b8be5a838095bc30261
Max Zaitsev <zaytsev.max90@gmail.com>
2019-03-27 10:03:44 +0300

    добавляем тип цен

--- catalog/index.php
+++ catalog/index.php
@@ -107,6 +107,7 @@ include_once $_SERVER["DOCUMENT_ROOT"] . "/apls_lib/catalog/sections/APLS_Catalo
 			2 => "Ср. оптовая",
 			3 => "Оптовая",
 			4 => "Кр. оптовая",
+			5 => "ИМ",
 		),
 		"USE_PRICE_COUNT" => "N",
 		"SHOW_PRICE_COUNT" => "1",
