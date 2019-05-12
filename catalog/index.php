f94b15e10c7b360529806d2f7ee2edf8cf46e494
Aleks Melkov <alexmelkov@mail.ru>
2019-05-12 11:34:08 +0300

    Добавление склада в компонент Каталог

--- catalog/index.php
+++ catalog/index.php
@@ -182,7 +182,8 @@ include_once $_SERVER["DOCUMENT_ROOT"] . "/apls_lib/catalog/sections/APLS_Catalo
 			16 => "68",
 			17 => "69",
 			18 => "70",
-			19 => "75",
+			19 => "73",
+			20 => "75",
 		),
 		"USE_MIN_AMOUNT" => "N",
 		"USER_FIELDS" => array(
