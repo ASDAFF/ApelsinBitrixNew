e5e71e74bba3ed30b52a0448767b48f851dfb077
Aleks Melkov <alexmelkov@mail.ru>
2019-10-15 12:00:10 +0300

    Изменяем телефон в шапке

--- include/schedule.php
+++ include/schedule.php
@@ -1,6 +1,6 @@
 <div class="main_phone">
     <div class='title'>Интернет магазин:</div>
-    <div class='phone'><span>8 (800) </span>550-54-43</div>
+    <div class='phone'><span>8 (4912) </span>24-92-83</div>
 </div>
 <div class='time'>
     <div><span>ПН-ПТ</span> 09:00 - 20:00</div>
