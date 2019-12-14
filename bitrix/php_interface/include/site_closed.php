91ae07ae9bcb3320dc663c45a18771b26e07e90a
Max Zaitsev <mzaitsev@compuproject.com>
2019-12-14 18:06:48 +0300

    Новое окно для закрытого сайта

--- bitrix/php_interface/include/site_closed.php
+++ bitrix/php_interface/include/site_closed.php
@@ -3,12 +3,12 @@ $APPLICATION->SetTitle("Сайт закрыт");
 ?>
 
 <h1>На сайте проводятся технические работы. Доступ закрыт.</h1>
-    <h2>По всем вопросам обращайтесь по номеру телефона 8 (4912) 240-220</h2>
-<?
-$APPLICATION->IncludeComponent("bitrix:system.auth.form",".default",Array(
-        "REGISTER_URL" => "",
-        "FORGOT_PASSWORD_URL" => "",
-        "PROFILE_URL" => "",
-        "SHOW_ERRORS" => "Y"
-    )
-);?>
+<h2>По всем вопросам обращайтесь на почту info@apelsin.ru</h2>
+<?//
+//$APPLICATION->IncludeComponent("bitrix:system.auth.form",".default",Array(
+//        "REGISTER_URL" => "",
+//        "FORGOT_PASSWORD_URL" => "",
+//        "PROFILE_URL" => "",
+//        "SHOW_ERRORS" => "Y"
+//    )
+//);?>
